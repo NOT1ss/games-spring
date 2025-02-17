8c7857246f5f32f8dba0a5e2ea9e86f9edbaa17e
Nelson Trindade <nelsonlouren14@gmail.com>
2025-02-17 17:46:01 +0000

    atualizacao1702

--- app/src/main/model/Categoria.java
+++ app/src/main/model/Categoria.java
@@ -8,15 +8,42 @@ import java.util.set;
 //import jakarta.persistence.GenerationType;
 //import jakarta.persistence.GeneratedValue;
 //import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "categorias")
-import jakarta.persistence.*;
+
 public class Categoria{
     @id
     @GeneratedValue(strategy = GenerationType.INDENTITY)
     private long id;
     @Column (unique=true, nullable=false)
-
     private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet<>;
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos(){
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos){
+        this.jogos = jogos;
+    }
 }
